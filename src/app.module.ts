5441f1d59f7080647c8d76d83dabf5a387a5adf4
Raphael Faria <raphaelsimoes@gmail.com>
2022-12-14 00:07:27 -0300

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule { }
