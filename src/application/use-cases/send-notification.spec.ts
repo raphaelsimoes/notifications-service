d29d220ead49c5c280a95bebaeafa4887017c47a
Raphael Faria <raphaelsimoes@gmail.com>
2022-12-15 00:01:04 -0300

    Aula 2

---
+++ src/application/use-cases/send-notification.spec.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+
+import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notifications-repository";
+import { SendNotification } from "./send-notification";
+
+describe('Send Notification', () => {
+
+    it('should be able to send a notification', async () => {
+
+        const notificationsRepository = new InMemoryNotificationsRepository()
+
+        const sendNotification = new SendNotification(notificationsRepository);
+
+        const { notification } = await sendNotification.execute({
+            content: 'This is a notification',
+            category: 'social',
+            recipientId: 'example-recipient-id',
+        });
+
+        expect(notificationsRepository.notifications).toHaveLength(1);
+
+        expect(notificationsRepository.notifications[0]).toEqual(notification);
+
+    });
+
+});
